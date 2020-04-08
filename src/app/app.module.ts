967ed3f99608e53364a75f1c54dee3fbeb4506db
zjimenez110492 <zjimenez@unicauca.edu.co>
2020-04-08 00:22:19 -0500

    08042020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PersonaComponent } from './persona/persona.component';
+import { PersonasComponent } from './personas/personas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,PersonasComponent,
+    PersonaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/persona/persona.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-persona',
+  templateUrl: './persona.component.html',
+  styleUrls: ['./persona.component.css']
+})
+export class PersonaComponent {
+
+  constructor() { }
+
+ 
+
+}
