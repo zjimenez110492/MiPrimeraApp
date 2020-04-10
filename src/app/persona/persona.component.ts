7f43c5bc94c23f79be6e45fa7925b64f7c3cc381
Jhonatan Zuniga <jhonatan3506@gmail.com>
2020-04-09 20:45:34 -0500

    Mostrar personas desde arreglo, directivas *¨ngIf, *ngFor

--- src/app/persona/persona.component.ts
+++ src/app/persona/persona.component.ts
@@ -7,13 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonaComponent {
 
-  
-  nombrePersona:string="Juan";
-  apellidoPersona: string="Perez";
-  edad:number=28;
- getEdad()
- {
-   return this.edad;
- }
-
+ 
+ public  nombrePersona:string;
+  public apellidoPersona: string;
+  public edad:number;
+  constructor(nombre:string,apellido:string, edad:number)
+  {
+    this.nombrePersona=nombre;
+    this.apellidoPersona=apellido;
+    this.edad=edad;
+  }
+  getEdad():number
+  {
+    return this.edad;
+  }
+  getApellidoPersona():string
+  {
+    return this.apellidoPersona;
+  }
+  getNombrePersona():string
+  {
+    return this.nombrePersona;
+  }
 }
--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-
+import { PersonaComponent } from '../persona/persona.component';
 @Component({
     selector: 'app-personas',
     templateUrl: './personas.component.html',
@@ -8,17 +8,30 @@ import { Component } from "@angular/core";
 export class PersonasComponent
 {
     agregarPersona=false;
+    personaCreada=false;
     agregarPersonaStatus="No se ha agregado ninguna persona";
     tituloPersona="Ingeniero";
-   constructor()
+    arrayPersonas: PersonaComponent[]=[new PersonaComponent("Jhonatan","Zuniga",27), new PersonaComponent("Julieta","Zuniga",4), new PersonaComponent("Maritza","Jimenez",46)];
+    nombreInput:string;
+    apellidoInput:string;
+    edadInput:number;
+    constructor()
    {
        setTimeout(
         ()=>{this.agregarPersona=true;}   
         ,3000);
    }
+   onAgregarPersona():void
+   {
+    let persona1= new PersonaComponent(this.nombreInput,this.apellidoInput,this.edadInput);
+    this.arrayPersonas.push(persona1);
+    this.personaCreada=true;
+    console.log("HOLA, YA SE AGREGÓ LA PERSONA"+this.nombreInput);
+   }
    onCrearPersona()
    {
         this.agregarPersonaStatus="Persona Agregada";
+        this.personaCreada=true;
    }
    onModificarPersona(event: Event)
    {
